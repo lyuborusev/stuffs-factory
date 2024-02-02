eeeb52cc9a5a77285dabfc3eb44ed83644dda70d
lyuborusev <lyubomir_rusev@yahoo.com>
2024-02-03 01:46:28 +0200

    Cleanup leftovers

--- api/interfaces/user.ts
+++
@@ -1,9 +0,0 @@
-enum UserType {
-    Architect = 1,
-    Designer = 2,
-    Builder = 3,
-}
-
-interface User {
-    type: UserType
-}
--- api/resources/stuff/stuff.router.ts
+++
@@ -1,4 +0,0 @@
-import StuffController from './stuff.controller'
-import RouterCRUD from '../../interfaces/router'
-
-//export default new RouterCRUD(new StuffController).getRouter();
